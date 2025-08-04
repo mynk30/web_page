ea7c2ce8c8c3031a64431e361e64e85e4cfb2321
Anjali <anjali@gmail.com>
2025-08-05 01:32:35 +0530

    email otp pending

--- account/dashboard.php
+++ account/dashboard.php
@@ -73,7 +73,7 @@ $browserLogger->log("Recent applications fetched: " . json_encode($recentApplica
         </div>
         
         <!-- Main content -->
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Dashboard</h1>
                 <div class="btn-toolbar mb-2 mb-md-0">
@@ -226,7 +226,7 @@ $browserLogger->log("Recent applications fetched: " . json_encode($recentApplica
                     </div>
 
                     <div class="mb-3">
-                        <label class="form-label">Email Address</label>
+                        <label class="form-label">Email Address</label> 
                         <input disabled value="<?php echo htmlspecialchars($_SESSION['email']); ?>" type="email" name="email" class="form-control" required>
                     </div>
                 
